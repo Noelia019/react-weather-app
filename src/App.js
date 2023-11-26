9d973fa623ade2d4c80b6c55600455476ceebd5f
Noelia019 <noeliaparina19@gmail.com>
2023-11-26 19:44:22 +0000

    Added the footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <h1>React app</h1>
+      <h1>React App</h1>
+      <footer>
+        This project was coded by Noelia Parina and is open-sourced on{" "}
+        <a href="https://github.com/Noelia019/react-weather-app">GitHub</a>.
+      </footer>
     </div>
   );
 }
