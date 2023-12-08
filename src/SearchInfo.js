8ff104a658c52a332179eb829d3fa91fcfb70616
Noelia019 <noeliaparina19@gmail.com>
2023-12-08 17:00:21 +0000

    Fixedcode for the WeatherIcon component + adding weather forecast layout

--- src/SearchInfo.js
+++ src/SearchInfo.js
@@ -16,7 +16,7 @@ export default function SearchInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <WeatherIcon code="props.data.icon" className="float-left" />
+            <WeatherIcon code={props.data.icon} className="float-left" />
 
             <span className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
