1b8c2033f485d2ec24a9fa320d3f9fb4572d7845
Noelia019 <noeliaparina19@gmail.com>
2023-12-08 12:52:28 +0000

    Added unit conversion

--- src/SearchInfo.js
+++ src/SearchInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function SearchInfo(props) {
   return (
@@ -18,10 +19,7 @@ export default function SearchInfo(props) {
             <WeatherIcon code="props.data.icon" className="float-left" />
 
             <span className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </span>
           </div>
         </div>
